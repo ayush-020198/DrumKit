aefa9c8233c30c53ff52a7448d962b8b0ebc7428
Ayush Shrivastava <ayush.shrivastavaujn@gmail.com>
2020-06-24 21:11:36 +0530

    adding sound and images

--- index.js
+++ index.js
@@ -2,7 +2,39 @@ var drumClickLength = document.querySelectorAll(".drum").length;
 
 for(var i=0; i<drumClickLength; i++){
     document.querySelectorAll(".drum")[i].addEventListener("click", function () {
-        alert("I got clicked");
+      var buttonSoundWord = this.innerHTML;
+      switch(buttonSoundWord){
+            case "w":
+                var audio = new Audio(src="sounds/tom-1.mp3");
+                audio.play();
+                break;
+            case "a":
+                var audio = new Audio(src="sounds/tom-2.mp3");
+                audio.play();
+                break;
+            case "s":
+                var audio = new Audio(src="sounds/tom-3.mp3");
+                audio.play();
+                break;      
+            case "d":
+                var audio = new Audio(src="sounds/tom-4.mp3");
+                audio.play();
+                break;   
+            case "j":
+            var audio = new Audio(src="sounds/snare.mp3");
+            audio.play();
+            break;
+            case "k":
+                var audio = new Audio(src="sounds/crash.mp3");
+                audio.play();
+                break;      
+            case "l":
+                var audio = new Audio(src="sounds/kick-bass.mp3");
+                audio.play();
+                break;       
+                 
+
+      }
     })
 }
 
